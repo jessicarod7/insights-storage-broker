30b0c0e5649d709854e9262fb541068e56f78aa9
Jessica Rodriguez <jrodri@redhat.com>
2025-07-14 13:41:53 -0400

    [RHCLOUD-40146] Bump python deps

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ setup(
         "prometheus-client==0.21.1",
         "logstash-formatter==0.5.17",
         "watchtower==3.4.0",
-        "confluent-kafka==2.10.0",
-        "boto3==1.38.18",
+        "confluent-kafka==2.11.0",
+        "boto3==1.39.4",
         "pyyaml==6.0.2",
-        "attrs==18.2.0",
-        "app-common-python==0.2.3"
+        "attrs==25.3.0",
+        "app-common-python==0.2.8"
     ],
-    extras_require={"test": ["pytest>=5.4.1", "flake8>=3.7.9"]},
+    extras_require={"test": ["pytest>=8.0.0", "flake8>=7.3.0"]},
     entry_points={"console_scripts": ["storage_broker = storage_broker.app:main"]},
 )
